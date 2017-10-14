76cecc1f93b4674a6963d57db7572cdd19530bbd
John Tuttle <thejwt@gmail.com>
2017-10-14 17:40:06 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Pokemon
 
-  def initialize(name: "name",type: "type",db)
+  def initialize(name: "name",type: "type",db: = "db")
     @name = name
     @type = type
     @db = db
